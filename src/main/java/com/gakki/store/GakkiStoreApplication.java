b6463a019e1de0d3065706797083bc874e30767c
Daniel Shizuo <danielmakiyamap@gmail.com>
2026-09-13 20:10:13 -0300

    chore: estruturar projeto Spring Boot do backend

---
+++ src/main/java/com/gakki/store/GakkiStoreApplication.java
@@ -0,0 +1,12 @@
+package com.gakki.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GakkiStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GakkiStoreApplication.class, args);
+    }
+}
